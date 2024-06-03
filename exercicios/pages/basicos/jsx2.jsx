629940ea0749fcb2e3bb7767978bfab64ba0c8b6
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2024-06-03 12:04:34 -0300

    Integrando um Array dentro de um conteúdo HTML

---
+++ exercicios/pages/basicos/jsx2.jsx
@@ -0,0 +1,18 @@
+export default function jsx2() {
+
+    const array = [
+        '#01',
+        '#02',
+        '#03',
+        '#04',
+        '#05',
+    ]
+    
+
+    const conteudo = (
+        <div>
+            <h1>JSX {array[1]}</h1>
+        </div>
+    )
+    return conteudo
+}
