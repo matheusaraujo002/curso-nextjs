274d50d71b1e09020d61699150b09c22258d540d
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2024-07-31 12:32:42 -0300

    Projeto Tabuleiro

---
+++ tabuleiro/components/Tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from "@/components/Linha";
+
+export default function Tabuleiro() {
+    return (
+        <div>
+            <Linha />
+            <Linha preto />
+            <Linha />
+            <Linha preto />
+            <Linha />
+            <Linha preto />
+            <Linha />
+            <Linha preto />
+        </div>
+    )
+}
