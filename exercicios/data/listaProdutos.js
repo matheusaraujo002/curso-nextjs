2ffe4c13f3c5c2a828d9d59a1012276bcae14feb
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2025-01-23 14:04:35 -0300

    Objetos em JS

---
+++ exercicios/data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "../model/produto"
+
+export default [
+    new Produto(1, "iPad", 7530),
+    new Produto(2, "iPhone", 4530),
+    new Produto(2, "Monitor 27", 1300),
+    new Produto(2, "Computador Positivo", 100),
+]
